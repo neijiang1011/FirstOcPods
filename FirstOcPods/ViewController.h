7163c67b53e53acb130b66ab944614726b40db6f
yetao ye <yetaoye@yetaodemacbook-pro.local>
2018-01-24 10:37:54 +0800

    Initial Commit

---
+++ FirstOcPods/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FirstOcPods
+//
+//  Created by yetao ye on 2018/1/24.
+//  Copyright © 2018年 com.changhong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ FirstOcPods/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  FirstOcPods
+//
+//  Created by yetao ye on 2018/1/24.
+//  Copyright © 2018年 com.changhong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
